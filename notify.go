38425a640c38122be8b7ec4e8aa2b5b2c556b6bc
Aaron Schlesinger <aaron@ecomaz.net>
2021-03-26 14:47:35 -0700

    adding tests, comments and a ctor

--- notify.go
+++ notify.go
@@ -6,16 +6,25 @@ import "sync"
 // Listener goroutines must accept the notification within a pre-defined amount of time or the notification will
 // be aborted. Once a notification goes out, all listener goroutines are automatically unregistered.
 //
-// All operations on Reusables are concurrency-safe
-type Reusable struct {
+// All operations on Reusables are concurrency-safe.
+//
+// Create a new SingleUseBroadcast with NewSingleUseBroadcast.
+type SingleUseBroadcast struct {
 	chsM *sync.RWMutex
 	chs  []chan struct{}
+}
+
+func NewSingleUseBroadcast() *SingleUseBroadcast {
+	return &SingleUseBroadcast{
+		chsM: new(sync.RWMutex),
+		chs:  nil,
+	}
 }
 
 // Register indicates that the caller would like to listen to r. The returned channel will be closed when
 // a notification arrives. After the notification, this channel is closed and nothing will happen
 // to it again. If you want to re-register, call this function again and use the new return channel.
-func Register(r *Reusable) <-chan struct{} {
+func Register(r *SingleUseBroadcast) <-chan struct{} {
 	r.chsM.Lock()
 	defer r.chsM.Unlock()
 	newCh := make(chan struct{})
@@ -23,8 +32,9 @@ func Register(r *Reusable) <-chan struct{} {
 	return newCh
 }
 
-// Notify notifies all listeners on r. After Notify returns, there will be no listeners on r.
-func Notify(r *Reusable) {
+// Notify notifies all listeners on r. After Notify returns, all channels previously issued by Register
+// will be closed and all listeners will be notified
+func Notify(r *SingleUseBroadcast) {
 	r.chsM.Lock()
 	defer r.chsM.Unlock()
 	for _, ch := range r.chs {
---
+++ notify_test.go
@@ -0,0 +1,81 @@
+package notify
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+)
+
+func iter(n int, fn func(i int) error) error {
+	for i := 0; i < n; i++ {
+		if err := fn(i); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func parFunc(i int, fn func(i int) error) func() error {
+	return func() error {
+		return fn(i)
+	}
+}
+
+func iterPar(ctx context.Context, n int, fn func(i int) error) error {
+	errgrp, _ := errgroup.WithContext(ctx)
+	for i := 0; i < n; i++ {
+		errgrp.Go(parFunc(i, fn))
+	}
+	return errgrp.Wait()
+}
+
+func TestBroadcast(t *testing.T) {
+	ctx := context.Background()
+	r := require.New(t)
+
+	b := NewSingleUseBroadcast()
+
+	// register a bunch of listeners
+	const numListeners = 10
+	chans := make([]<-chan struct{}, numListeners)
+	iter(10, func(i int) error {
+		chans[i] = Register(b)
+		return nil
+	})
+
+	// make sure they all don't receive before calling notify
+	iterPar(ctx, 10, func(i int) error {
+		t := time.NewTimer(200 * time.Millisecond)
+		ch := chans[i]
+		select {
+		case <-ch:
+			return fmt.Errorf("signal %d received before notify", i)
+		case <-t.C:
+			return nil
+		}
+	})
+
+	notifiedCh := make(chan struct{})
+	go func() {
+		time.Sleep(1 * time.Second)
+		Notify(b)
+		close(notifiedCh)
+	}()
+	<-notifiedCh
+
+	iter(numListeners, func(i int) error {
+		ch := chans[i]
+		t := time.NewTimer(2 * time.Second)
+		defer t.Stop()
+		select {
+		case <-ch:
+		case <-t.C:
+			r.Fail("didn't receive on notified channel within 1s")
+		}
+		return nil
+	})
+}
